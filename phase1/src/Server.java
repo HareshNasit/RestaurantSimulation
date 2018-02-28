f3f8c4f9637de2274f4b7ad88e3c216a2fa9d0f5
Akarsh <akarsh.sharma@mail.utoronto.ca.here>
2018-02-28 15:04:46 -0500

    Declared variables in Server class.

--- phase1/src/Server.java
+++ phase1/src/Server.java
@@ -1,2 +1,9 @@
+import java.util.ArrayList;
+
 public class Server {
+    private String name;
+    private boolean isWorker;
+    protected ArrayList<Dish> orders;
+    public Server(String name, boolean isWorker){}
+
 }
