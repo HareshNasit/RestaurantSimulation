e8513996c2319ab4137cf356ad7be9c9e0fba674
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2018-02-28 16:03:49 -0500

    Rough methods added to class Cook.

--- phase1/src/Cook.java
+++ phase1/src/Cook.java
@@ -1,9 +1,22 @@
+import java.util.ArrayList;
+
 /*
  * A cook decides whether the dish can be prepared and prepares the dish.
  */
 public class Cook {
     private static int numOfCooks;
     private String name; // Name of the cook.
-    private boolean hasDishIngredients; // whether the dish can be prepared.
-    
+    private ArrayList<Dish> dishesInMaking; // Number of dishes this cook is preparing.
+    private ArrayList<Dish> dishesReady; // Number of dishes ready to be delivered.
+
+    public Cook(){
+        numOfCooks++;
+        this.dishesReady = new ArrayList<>();
+        this.dishesInMaking = new ArrayList<>();
+    }
+    public void prepareDish(Dish dish){}
+
+    public void dishReady(Dish dish){
+        dishesReady.add(dish);
+    }
 }
