81a31d2320766615c4fb4062a500a6d81d17b43a
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2018-02-28 15:29:28 -0500

    Instant variables added to Cook class.

--- phase1/src/Cook.java
+++ phase1/src/Cook.java
@@ -1,2 +1,9 @@
+/*
+ * A cook decides whether the dish can be prepared and prepares the dish.
+ */
 public class Cook {
+    private static int numOfCooks;
+    private String name; // Name of the cook.
+    private boolean hasDishIngredients; // whether the dish can be prepared.
+    
 }
