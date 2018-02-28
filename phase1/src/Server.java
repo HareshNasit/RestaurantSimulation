d873e7a4a907a0d37a108de572423f5f541d9c79
Akarsh <akarsh.sharma@mail.utoronto.ca.here>
2018-02-28 17:03:08 -0500

    Added methods to the Server class.

--- phase1/src/Server.java
+++ phase1/src/Server.java
@@ -1,9 +1,31 @@
 import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Server class records orders taken from customers and relays them to the chef.
+ */
 
 public class Server {
-    private String name;
-    private boolean isWorker;
-    protected ArrayList<Dish> orders;
-    public Server(String name, boolean isWorker){}
+    private static int numberOfServers;
+    private String name; // name of a server
+    protected ArrayList<Dish> dishes; // list of dishes the server has taken from a table
+    protected ArrayList<Table> tables; // the list of tables in the restaurant
+
+    public Server(String name){
+        this.name = name;
+        numberOfServers++;
+    }
+
+    /**
+     * The server takes the order from a table and adds it to the list of orders to be cooked
+     * @param table the table that places the order
+     */
+    public void addOrder(Table table){}
+
+    /**
+     * The server serves the order to the table and removes it from the list of orders to be served
+     * @param table the table whose order is being served
+     */
+    public void serveOrder(Table table){}
 
 }
