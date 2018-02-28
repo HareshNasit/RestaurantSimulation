aa83ed562af85e6f134a92a4573087fc3686ab35
Adam Zartin <A.Zartin@mail.utoronto.ca>
2018-02-28 14:56:27 -0500

    Created constructor for Inventory class. Implemented addStock() and removeStock() methods for the inventory class.

--- phase1/src/Inventory.java
+++ phase1/src/Inventory.java
@@ -1,2 +1,38 @@
+import java.util.HashMap;
+
+/**
+ * Inventory uses a a HashMap data structure where the key is a String ingredient and value is the
+ * number of that ingredient available.
+ */
 public class Inventory {
+
+  private HashMap<String, Integer> invetory;
+
+  /**
+   * Constructs a new Inventory object
+   */
+  Inventory() {
+    invetory = new HashMap<String, Integer>();
+  }
+
+  /**
+   * Increases the current stock of String ingredient by int amount
+   *
+   * @param ingredient The ingredient that is to be added to
+   * @param amount The amount by which the ingredient stock is going to be increased by
+   */
+  public void addStock(String ingredient, int amount) {
+    invetory.put(ingredient, invetory.get(ingredient) + amount);
+  }
+
+  /**
+   * Reduces teh current stock of String ingredient by int amount
+   *
+   * @param ingredient The ingredient that is to be added to
+   * @param amount The amount by which the ingredient stock is going to be reduced by
+   */
+  public void removeStock(String ingredient, int amount) {
+    invetory.put(ingredient, invetory.get(ingredient) - amount);
+  }
+
 }
