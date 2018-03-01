2f0579cbd303effded77bc1990fa4db81f73a2a5
Akarsh <akarsh.sharma@mail.utoronto.ca.here>
2018-03-01 11:52:44 -0500

    Added starting variables and constructor to the Table class.

--- phase1/src/Table.java
+++ phase1/src/Table.java
@@ -1,2 +1,17 @@
+/**
+ * The table is where the customers sit and order food and also where their cooked food arrives.
+ */
+
 public class Table {
+    private String tableNumber; // the id number of the table
+    private int tableSize; // the number of people who can sit on this table
+    private boolean isOccupied; // of the table already has customers
+
+    public Table(String tableNumber, int tableSize, boolean isOccupied){
+        this.tableNumber = tableNumber;
+        this.tableSize = tableSize;
+        this.isOccupied = isOccupied;
+    }
+
+    // what methods does table have?
 }
