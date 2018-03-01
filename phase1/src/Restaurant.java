e4b8451c69f14e9cb89cc7f8c96b54bb593fc5a8
Yosef <yosef.leibman@mail.utoronto.ca>
2018-03-01 16:36:24 -0500

    Created the constructor for Restaurant class and added two instance variables (Inventory and Menu)

--- phase1/src/Restaurant.java
+++ phase1/src/Restaurant.java
@@ -1,2 +1,14 @@
+/**
+ * This is the Restaurant class. This is where all the moving parts move around.
+ */
 public class Restaurant {
+
+  private Inventory inventory;
+  private Menu menu;
+
+  public Restaurant() {
+    this.inventory = new Inventory();
+    this.menu = new Menu();
+  }
+
 }
