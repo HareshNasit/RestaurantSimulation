46d6cbff3e86a8eb40b7aadbdc29521cfba4cbd1
Adam Zartin <A.Zartin@mail.utoronto.ca>
2018-03-01 16:58:53 -0500

    Implemented main methods for ServingTable

--- phase1/src/ServingTable.java
+++ phase1/src/ServingTable.java
@@ -1,15 +1,28 @@
+import java.util.LinkedList;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
 
 public class ServingTable {
 
-  ArrayList<Dish> dishesToBeCooked;
-  ArrayList<Dish> dishesToBeServed;
+  PriorityQueue<Dish> dishesToBeCooked;
+  PriorityQueue<Dish> dishesToBeServed;
 
   ServingTable() {
-    dishesToBeCooked = new ArrayList<Dish>();
-    dishesToBeServed = new ArrayList<Dish>();
-
+    dishesToBeCooked = new PriorityQueue<>();
+    dishesToBeServed = new PriorityQueue<>();
   }
 
+  public void addToBeCooked(ArrayList<Dish> order) {
+    dishesToBeCooked.addAll(order);
+  }
+
+  public void addToBeServed(ArrayList<Dish> order) {
+    dishesToBeServed.addAll(order);
+  }
+
+  public Dish getDishFromServingTable() {
+    return dishesToBeCooked.poll();
+  }
 
 }
