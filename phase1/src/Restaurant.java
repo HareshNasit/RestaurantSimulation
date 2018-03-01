96f7f5e6357e18b0bfba9e2aae39b74b1fd40930
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2018-03-01 16:57:03 -0500

    Variable added to Restaurant class and few methods as well.

--- phase1/src/Restaurant.java
+++ phase1/src/Restaurant.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * This is the Restaurant class. This is where all the moving parts move around.
  */
@@ -5,10 +7,17 @@ public class Restaurant {
 
   private Inventory inventory;
   private Menu menu;
-
+  private static ArrayList<IWorker> workers;
   public Restaurant() {
     this.inventory = new Inventory();
     this.menu = new Menu();
+    workers = new ArrayList<>();
+  }
+  public void addWorker(IWorker worker){
+      workers.add(worker);
   }
 
+    public  ArrayList<IWorker> getWorkers() {
+        return workers;
+    }
 }
