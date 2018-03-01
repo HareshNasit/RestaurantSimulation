38529f78364fd0183d3335cb9831f162f0814db6
Adam Zartin <A.Zartin@mail.utoronto.ca>
2018-03-01 16:16:41 -0500

    Removed unnecessary methods and imports from Menu.java

--- phase1/src/Menu.java
+++ phase1/src/Menu.java
@@ -1,8 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -58,24 +56,5 @@ public class Menu {
     return menuItems.size();
   }
 
-  public ArrayList<Dish> readMenuItems() {
-    ArrayList<Dish> menuItems = new ArrayList<Dish>();
-    try (BufferedReader fileReader = new BufferedReader(new FileReader(FILENAME))) {
-      String line = fileReader.readLine();
-      while (line != null) {
-        int itemID = Integer.valueOf(line.split("#")[0].trim());
-        String name = line.split("#")[1].trim();
-        int cookTime = Integer.valueOf(line.split("#")[2].trim());
 
-        HashMap<String, int[]> ingredients = new HashMap<String, int[]>();
-        // Create ingredients hashmap here
-
-        Dish dish = new Dish(name, itemID, cookTime, ingredients);
-      }
-
-    } catch (Exception e) {
-    }
-
-    return menuItems;
-  }
 }
