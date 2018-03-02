6331e97805fa7a22532b6404bf247cbda1353d25
Yosef <yosef.leibman@mail.utoronto.ca>
2018-03-02 14:48:38 -0500

    Created methods for DishIngredient. Can now add and subtract amounts and check if an amount being added or subtracted is valid.

--- phase1/src/DishIngredient.java
+++ phase1/src/DishIngredient.java
@@ -1,14 +1,67 @@
 public class DishIngredient {
 
-    private String name;
-    private int amount;
-    private int upperAmount;
-    private int lowerAmount;
+  private String name;
+  private int amount;
+  private int upperAmount;
+  private int lowerAmount;
 
-    public DishIngredient(String name, int amount, int upperAmount, int lowerAmount){
-        this.name = name;
-        this.amount = amount;
-        this.upperAmount = upperAmount;
-        this.lowerAmount = lowerAmount;
-    }
+  public DishIngredient(String name, int amount, int upperAmount, int lowerAmount) {
+    this.name = name;
+    this.amount = amount;
+    this.upperAmount = upperAmount;
+    this.lowerAmount = lowerAmount;
+  }
+
+  /**
+   * Adds an amount to the ingredient.
+   *
+   * @param amount amount added to the ingredient.
+   */
+  public void addAmount(int amount) {
+    this.amount += amount;
+  }
+
+  /**
+   * Adds an amount to the ingredient.
+   *
+   * @param amount amount subtracted from the ingredient.
+   */
+  public void subtractAmount(int amount) {
+    this.amount -= amount;
+  }
+
+  /**
+   * Checks if an amount can be added to the dish's ingredient
+   *
+   * @param amount amount being added
+   * @return
+   */
+  public boolean amountCanBeAdded(int amount) {
+    return this.amount + amount <= upperAmount;
+  }
+
+  /**
+   * Checks if an amount can be subtracted to the dish's ingredient
+   *
+   * @param amount amount being subtracted
+   * @return
+   */
+  public boolean amountCanBeSubtracted(int amount) {
+    return this.amount - amount >= lowerAmount;
+  }
+
+  /** @return */
+  @Override
+  public String toString() {
+    return "The "
+        + this.name
+        + " for the dish has "
+        + "(amount: "
+        + this.amount
+        + " lower bound: "
+        + this.lowerAmount
+        + " upper bound: "
+        + this.upperAmount
+        + ")";
+  }
 }
