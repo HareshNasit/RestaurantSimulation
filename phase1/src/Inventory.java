0d8323bf49f7fdb8082d7eb580b9f3d3dc957f0e
Adam Zartin <A.Zartin@mail.utoronto.ca>
2018-03-01 20:24:30 -0500

    Reformatted inventory data structure for new key and value pairings. Value in hashmap is now a double list. Remove Ingredient.java as it is no longer needed

--- phase1/src/Inventory.java
+++ phase1/src/Inventory.java
@@ -1,9 +1,11 @@
+import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.Scanner;
 
 /**
  * Inventory uses a a HashMap data structure where the key is a String ingredient and value is the
@@ -11,14 +13,15 @@ import java.io.FileReader;
  */
 public class Inventory {
 
-  private final String FILENAME = "Inventory.txt";
-  private HashMap<String, Integer> inventory;
+  private final String INVENTORYFILE = "Inventory.txt";
+  private final String REQUESTSFILE = "request.txt";
+  private HashMap<String, double[]> inventory;
 
   /**
    * Constructs a new Inventory object
    */
   Inventory() {
-    inventory = new HashMap<String, Integer>();
+    inventory = new HashMap<String, double[]>();
   }
 
   /**
@@ -30,9 +33,9 @@ public class Inventory {
    */
   public void addStock(String ingredient, int amount) {
     if (inventory.get(ingredient) != null) {
-      inventory.put(ingredient, inventory.get(ingredient) + amount);
+      inventory.get(ingredient)[1] += amount;
     } else {
-      inventory.put(ingredient, amount);
+      inventory.put(ingredient, new double[]{0, amount, 0});
     }
 
 
@@ -45,8 +48,8 @@ public class Inventory {
    * @param amount The amount by which the ingredient stock is going to be reduced by
    */
   public void removeStock(String ingredient, int amount) {
-    if (amount <= inventory.get(ingredient)) {
-      inventory.put(ingredient, inventory.get(ingredient) - amount);
+    if (amount <= inventory.get(ingredient)[1]) {
+      inventory.get(ingredient)[1] -= amount;
     }
   }
 
@@ -55,11 +58,16 @@ public class Inventory {
    */
   public void writeToInventory() {
     // Open the file for writing and write to it.
-    try (PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(FILENAME)))) {
+    try (PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(INVENTORYFILE)))) {
       for (String ingredient : inventory.keySet()) {
+
         StringBuilder line = new StringBuilder(ingredient);
-        line.append("#" + inventory.get(ingredient));
+        String price = String.valueOf(inventory.get(ingredient)[0]);
+        String amount = String.valueOf(inventory.get(ingredient)[1]);
+        String lowerBounds = String.valueOf(inventory.get(ingredient)[2]);
+        line.append("#" + price + "#" + amount + "#" + lowerBounds);
         out.println(line);
+
       }
     } catch (Exception e) {
 
@@ -71,12 +79,15 @@ public class Inventory {
    * Reads the inventory from Inventory.txt
    */
   public void readInventory() {
-    try (BufferedReader fileReader = new BufferedReader(new FileReader(FILENAME))) {
+    try (BufferedReader fileReader = new BufferedReader(new FileReader(INVENTORYFILE))) {
       String line = fileReader.readLine();
       while (line != null) {
+
         String ingredient = line.split("#")[0];
-        int amount = Integer.valueOf(line.split("#")[1]);
-        inventory.put(ingredient, amount);
+        double price = Double.valueOf(line.split("#")[1]);
+        double amount = Double.valueOf(line.split("#")[2]);
+        double lowerBound = Double.valueOf(line.split("#")[3]);
+        inventory.put(ingredient, new double[]{price, amount, lowerBound});
         line = fileReader.readLine();
       }
 
