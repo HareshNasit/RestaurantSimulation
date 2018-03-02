ef3b19b6fe8a5ff228b6e6b0ea825c9b7cd192cb
Adam Zartin <A.Zartin@mail.utoronto.ca>
2018-03-02 16:54:48 -0500

    testsuite

---
+++ phase1/src/testSuite.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+
+public class testSuite {
+
+  public static void main(String[] args) {
+    HashMap<String, Integer> practice1 = new HashMap<String, Integer>();
+
+  }
+
+
+}
