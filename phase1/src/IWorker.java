d58c05721e11ece9bdd3a17bd12c6749ec9aeb37
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2018-03-03 14:52:13 -0500

    Changes in IWorker

--- phase1/src/IWorker.java
+++ phase1/src/IWorker.java
@@ -4,10 +4,6 @@ public interface IWorker {
    * Check if this IWorker is currently occupied or not
    */
   boolean isOccupied();
+  void addIngredients(Inventory inventory, String ingredient, int amount);
 
-  /**
-   * When new stock of ingredients has been recieved, make this IWorker scan it to update the current Inventory
-   * @return
-   */
-  boolean scanStock();
 }
