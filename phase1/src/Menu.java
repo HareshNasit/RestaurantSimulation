cee7289688c01b42c21ee0706d857128897fa7a5
Yosef <yosef.leibman@mail.utoronto.ca>
2018-03-03 12:44:11 -0500

    Fixed bug where the Menu wouldn't create the dishes correctly (forgot to trim whitespace). Forgot to instantiate the menuItems dishes, now instantiated in the constructor.

--- phase1/src/Menu.java
+++ phase1/src/Menu.java
@@ -9,12 +9,13 @@ public class Menu {
   private final String FILENAME = "menu.txt";
   private ArrayList<Dish> menuItems;
 
+  /** Creates a Menu from the FILENAME txt file and adds all the dishes to an array. */
   public Menu() {
-
+    this.menuItems = new ArrayList<Dish>();
     createMenu();
   }
 
-  /** Iterates through all the lines in the txt file to produce each dish on the menu */
+  /** Iterates through all the lines in the txt file to produce each dish on the menu. */
   private void createMenu() {
     // Aggregate the data
     // Each MenuItem takes in a line from the menu.txt
@@ -32,25 +33,26 @@ public class Menu {
       e.printStackTrace();
     }
   }
-  /** Creates a dish using a line from the txt file */
+
+  /** Creates a dish using a line from the txt file. */
   private void addDishToMenu(String line) {
-    String[] separatedByHashTag = line.split("#\\s");
+    String[] separatedByHashTag = line.split("#");
 
     String name = separatedByHashTag[1];
-    int id = Integer.parseInt(separatedByHashTag[0]);
-    int price = Integer.parseInt(separatedByHashTag[2]);
+    int id = Integer.parseInt(separatedByHashTag[0].trim());
+    int price = Integer.parseInt(separatedByHashTag[2].trim());
     HashMap<String, DishIngredient> ingredients = new HashMap<>();
 
     for (int i = 3; i < separatedByHashTag.length; i++) {
       String[] ingredient = separatedByHashTag[i].split(",");
       int[] bounds = new int[3];
       for (int k = 0; k < 3; k++) {
-        bounds[k] = Integer.parseInt(ingredient[k + 1]);
+        bounds[k] = Integer.parseInt(ingredient[k + 1].trim());
       }
       String ingredientName = ingredient[0];
-      int amount = Integer.parseInt(ingredient[1]);
-      int lowerAmount = Integer.parseInt(ingredient[2]);
-      int upperAmount = Integer.parseInt(ingredient[3]);
+      int amount = Integer.parseInt(ingredient[1].trim());
+      int lowerAmount = Integer.parseInt(ingredient[2].trim());
+      int upperAmount = Integer.parseInt(ingredient[3].trim());
 
       ingredients.put(
           ingredient[0], new DishIngredient(ingredientName, amount, lowerAmount, upperAmount));
