1d05774fd21ee94678dabab3ea9af2532b9854af
Adam Zartin <A.Zartin@mail.utoronto.ca>
2018-03-03 17:12:58 -0500

    -Reimplemented ServingTable and TableListener such that it can be passed on to Server and Cook

--- phase1/src/ServingTable.java
+++ phase1/src/ServingTable.java
@@ -6,23 +6,47 @@ public class ServingTable {
 
   PriorityQueue<Dish> dishesToBeCooked;
   PriorityQueue<Dish> dishesToBeServed;
+  ArrayList<ServingTableListener> servers;
+  ArrayList<ServingTableListener> cooks;
 
-  ServingTable() {
+  ServingTable(ArrayList<ServingTableListener> servers, ArrayList<ServingTableListener> cooks) {
     dishesToBeCooked = new PriorityQueue<>();
     dishesToBeServed = new PriorityQueue<>();
+    this.servers = servers;
+    this.cooks = cooks;
   }
 
+  /**
+   * Adds a table order to the list of dishes that needs to be cooked
+   */
   public void addToBeCooked(ArrayList<Dish> order) {
     dishesToBeCooked.addAll(order);
   }
 
+  /**
+   * Add the n
+   * @param order
+   */
   public void addToBeServed(ArrayList<Dish> order) { dishesToBeServed.addAll(order);
   }
 
-  public Dish getDishesToBeCooked() {
+  /**
+   * Returns the next dish that needs to be returned by the chef
+   * @return
+   */
+  public Dish getNextDishToBeCooked() {
     return dishesToBeCooked.poll();
   }
 
-  public Dish getDishesToBeServed(){ return dishesToBeServed.poll(); }
+
+  /**
+   * Remove Dish dish from list of dishes to be served. Doing so will mean that dish has been
+   * delivered to the table
+   * @param dish
+   * @return the Dish that has been cooked, and needs to be served to the customer
+   */
+  public Dish getDishToBeServed(Dish dish) {
+    return dishesToBeServed.poll();
+  }
 
 }
--- phase1/src/ServingTableListener.java
+++ phase1/src/ServingTableListener.java
@@ -1,5 +1,12 @@
+import java.util.ArrayList;
 import java.util.Observer;
 
 public interface ServingTableListener {
+
+  /**
+   * Notifies the appropriate ServingTableListener what to do
+   */
+  public void update();
+
 
 }
