80d36e785f2be3112b0d73f3e46b700f63c3206b
Akarsh <akarsh.sharma@mail.utoronto.ca.here>
2018-03-04 20:22:52 -0500

    Updated javadocs in class IWorker.

--- phase1/src/IWorker.java
+++ phase1/src/IWorker.java
@@ -5,6 +5,12 @@ public interface IWorker {
    */
   boolean isOccupied();
 
+  /**
+   * Records what ingredient is being added to the inventory and how many units are being added
+   * @param inventory The stored ingredients in the restaurant
+   * @param ingredient The ingredient being added to the inventory
+   * @param amount How much of that ingredient is being added
+   */
   void scanStock(Inventory inventory, String ingredient, int amount);
 
 }
