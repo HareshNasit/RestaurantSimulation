5a9488732b6fb11e0d8989894712fd513408d3ce
Akarsh <akarsh.sharma@mail.utoronto.ca.here>
2018-03-06 17:12:20 -0500

    Added the callManager method to class ServerListener.

--- phase1/src/ServerListener.java
+++ phase1/src/ServerListener.java
@@ -1,3 +1,8 @@
 public interface ServerListener {
 
+    /**
+     * Server calls manager if a customer has a problem with the food
+     * @param manager The manager that is being called
+     */
+    public void callManager(Manager manager);
 }
