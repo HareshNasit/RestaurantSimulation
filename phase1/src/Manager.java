a9e823b4ef33730d933364f6b86832659777a202
Akarsh <akarsh.sharma@mail.utoronto.ca.here>
2018-03-08 14:50:22 -0500

    Added a new String variable and removed a method from the Manager class. (Yosef)

--- phase1/src/Manager.java
+++ phase1/src/Manager.java
@@ -1,80 +1,61 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
 
 /*
  * A Manager manages the overall restaurant and gets the ingredients if there
  * is a shortage.
  */
 public class Manager implements IWorker, InventoryListener {
-  private String name;
-  private String requestIngredients = "request.txt";
+    private String name;
+    private String requestIngredients = "request.txt";
+    private String receivedShipments = "receivedShipments.txt";
 
-  public Manager(String name) {
-    this.name = name;
-  }
+    public Manager(String name) {
+        this.name = name;
+    }
     @Override
     public String getName() {
         return name;
     }
     /** Calls a server or a cook to collect the received ingredients. */
-  public IWorker callWorker(Restaurant restaurant, String name) {
-      return restaurant.MapOfWorkers().get(name);
-  }
-
-  @Override
-  public void notifyLowStock(String message) {
-    //to be replaced with gui notification
-    System.out.println(message);
-  }
-  /** The server receives and adds ingredients to the inventory. */
-  public void scanStock(Inventory inventory, String ingredient, int amount) {
-    inventory.addStock(ingredient, amount);
-  }
-  /** The Manager shuts the system and updates the inventory.txt file. */
-  public void shutDown(Inventory inventory) {
-    System.out.println("System Shutdown: Writing inventory to file");
-    inventory.writeToInventory();
-  }
-
-  public void confirmReceived(String ingredient, int amount) {
-    try (PrintWriter out =
-        new PrintWriter(new BufferedWriter(new FileWriter(requestIngredients)))) {
-      ArrayList<String> list = serializeRequests();
-      String request = "";
-      for (int i = 0; i < list.size()/4; i++) {
-          String ingredientName = list.get(i*4);
-          int quantity = Integer.parseInt(list.get(i*4+1));
-          String received = list.get(i*4+2);
-          String scanned = list.get(i*4+3);
-          if(ingredientName.equals(ingredient) && quantity == amount && received.equals("notreceived")){
-              request += ingredientName + "#" + quantity + "#received#" + scanned;
-          }else{
-              request += ingredientName + "#" + quantity + "#" + received + "#" + scanned;
-          }
-          out.println(request);
-          request = "";
-      }
-
-    } catch (Exception e) {
-
+    public IWorker callWorker(Restaurant restaurant, String name) {
+        return restaurant.MapOfWorkers().get(name);
     }
-  }
 
-  private ArrayList<String> serializeRequests() {
-    ArrayList<String> list = new ArrayList<>();
-    try (BufferedReader fileReader = new BufferedReader(new FileReader(requestIngredients))) {
-      String line = fileReader.readLine();
-      while (line != null) {
-        String[] elements = line.split("#");
-        list.add(elements[0]);
-        list.add(elements[1]);
-        list.add(elements[2]);
-        list.add(elements[3]);
-        line = fileReader.readLine();
-      }
-    } catch (java.io.IOException e) {
+    @Override
+    public void notifyLowStock(String message) {
+        //to be replaced with gui notification
+        System.out.println(message);
     }
-    return list;
-  }
+    /** The server receives and adds ingredients to the inventory. */
+    public void scanStock(Inventory inventory, String ingredient, int amount) {
+        inventory.addStock(ingredient, amount);
+    }
+    /** The Manager shuts the system and updates the inventory.txt file. */
+    public void shutDown(Inventory inventory) {
+        System.out.println("System Shutdown: Writing inventory to file");
+        inventory.writeToInventory();
+    }
+
+    /**
+     * The Manager inputs how much of an ingredient the Restaurant has received.
+     * This gets printed to a new txt. The manager will compare between the request txt and receivedShipments
+     * txt to see which shipments have been received. This is useful if a shipment has not come in, so the manager
+     * can send an email to the supplier.
+     * @param ingredient
+     * @param amount
+     */
+    public void confirmReceived(String ingredient, int amount) {
+        try (PrintWriter out =
+                     new PrintWriter(new BufferedWriter(new FileWriter(receivedShipments)))) {
+
+            String shipmentMessage = ingredient + ": " + amount;
+            out.println(shipmentMessage);
+
+        } catch (Exception e) {
+
+        }
+    }
+
+
+
 }
