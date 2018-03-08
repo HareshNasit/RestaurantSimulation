5f2e2c60db64a09260c02a54cb4a2b1f5c5c767e
Adam Zartin <A.Zartin@mail.utoronto.ca>
2018-03-08 15:40:34 -0500

    -added javadocs and reformatted code

--- phase1/src/InventoryListener.java
+++ phase1/src/InventoryListener.java
@@ -1,5 +1,9 @@
 public interface InventoryListener {
 
-  public void notifyLowStock(String message);
-
+  /**
+   * Notifies this inventory listener that something has changed about the inventory
+   *
+   * @param message message to display on listener
+   */
+  void notifyLowStock(String message);
 }
