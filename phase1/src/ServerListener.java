b85bd60dafc77522b7f9e099def47e812f3563c9
Adam Zartin <A.Zartin@mail.utoronto.ca>
2018-03-08 15:50:36 -0500

    -Removed not used files

--- phase1/src/ServerListener.java
+++
@@ -1,8 +0,0 @@
-public interface ServerListener {
-
-    /**
-     * Server calls manager if a customer has a problem with the food
-     * @param manager The manager that is being called
-     */
-    public void callManager(Manager manager);
-}
