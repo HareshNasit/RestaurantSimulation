fd5437c2522a79b1c77dbdb14d369210c9d6d937
Akarsh <akarsh.sharma@mail.utoronto.ca.here>
2018-03-15 16:55:46 -0400

    Updated and fixed the errors in OrderScreen.java

--- phase2/src/OrderScreen/OrderScreen.java
+++ phase2/src/OrderScreen/OrderScreen.java
@@ -1,10 +1,19 @@
 package OrderScreen;
 
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-
+import javafx.scene.control.ScrollPane;
 
 
 public class OrderScreen {
-
     public Label tableOrderTitle;
+    public ScrollPane tableOrderScrollPane;
+
+    public Label menuLabel;
+    public ScrollPane menuScrollPanel;
+    public Button addDish;
+    public Button removeDish;
+    public Button printBill;
+
+    public void handle(){};
 }
