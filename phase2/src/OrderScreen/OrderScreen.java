240ef8d03ed3bd4a1b1a328d396f1a0e3ed34857
Akarsh <akarsh.sharma@mail.utoronto.ca.here>
2018-03-15 16:06:23 -0400

    Tried fixing variable errors in class OrderScreen.

--- phase2/src/OrderScreen/OrderScreen.java
+++ phase2/src/OrderScreen/OrderScreen.java
@@ -3,11 +3,6 @@ package OrderScreen;
 import java.awt.*;
 
 public class OrderScreen {
-    public Button addDish;
-    public Button removeDish;
-    public Button printBill;
-    public ScrollPane tableOrderScrollPane;
-    public ScrollPane menuScrollPane;
-    public Label tableOrderLabel;
-    public Label menuLabel;
+   public Label tableOrderLabel;
+
 }
