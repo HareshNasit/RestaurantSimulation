9b293d86528e026d1bf80514e64b1247c0272619
Akarsh <akarsh.sharma@mail.utoronto.ca.here>
2018-03-15 15:10:44 -0400

    Added variables to the OrderScreen class.

--- phase2/src/OrderScreen/OrderScreen.java
+++ phase2/src/OrderScreen/OrderScreen.java
@@ -4,4 +4,10 @@ import java.awt.*;
 
 public class OrderScreen {
     public Button addDish;
+    public Button removeDish;
+    public Button printBill;
+    public ScrollPane tableOrderScrollPane;
+    public ScrollPane menuScrollPane;
+    public Label tableOrderLabel;
+    public Label menuLabel;
 }
