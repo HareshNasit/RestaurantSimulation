64a41272061a8d16f0cca94c9ae786897d11dafe
Akarsh <akarsh.sharma@mail.utoronto.ca.here>
2018-03-15 19:06:07 -0400

    Added code to the OrderScreenSuite class to make it display the GUI.

--- phase2/src/OrderScreen/OrderScreenSuite.java
+++ phase2/src/OrderScreen/OrderScreenSuite.java
@@ -1,4 +1,33 @@
 package OrderScreen;
 
-public class OrderScreenSuite {
+import Restaurant.Dish;
+import Restaurant.Menu;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class OrderScreenSuite extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Menu menu = new Menu();
+        Stage window = new Stage();
+
+        FXMLLoader loader = new  FXMLLoader(getClass().getResource("orders.fxml"));
+        Parent root = loader.load();
+
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle("Order Screen");
+        window.setScene(new Scene(root));
+        window.show();
+
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
