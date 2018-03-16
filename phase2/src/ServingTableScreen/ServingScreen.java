e8311a7a5815d128e84d36d351ca8fe5daf6c6d6
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2018-03-16 17:51:11 -0400

    Column values for the dishes taken into account.

--- phase2/src/ServingTableScreen/ServingScreen.java
+++ phase2/src/ServingTableScreen/ServingScreen.java
@@ -1,4 +1,50 @@
 package ServingTableScreen;
 
-public class ServingScreen {
+import Restaurant.Restaurant;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import Restaurant.Dish;
+import Restaurant.Table;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class ServingScreen implements Initializable {
+
+
+    public TableView cookTable;
+    public TableColumn TableCol;
+    public TableColumn DishCol1;
+    private ObservableList<Dish> dishes;
+
+    public void setCookTable(Restaurant restaurant) {
+        this.DishCol1.getTableView().setItems(getDishes(restaurant));
+    }
+    private ObservableList<Dish> getDishes(Restaurant restaurant) {
+        this.dishes = FXCollections.observableArrayList();
+        dishes.addAll(restaurant.getServingTable().getDishesToBeCooked());
+        return dishes;
+    }
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        TableCol.setCellValueFactory(new PropertyValueFactory<Dish, String>("tableName"));
+        //System.out.println(getCookTable());
+    }
+
+    public <T, S> TableColumn<S, T> getTableIDColumn() {
+        return TableCol;
+    }
+
+    public <T, S> TableColumn<S, T> getDishCol() {
+        return DishCol1;
+    }
+
+    public TableView getCookTable() {
+        return cookTable;
+    }
 }
--- phase2/src/ServingTableScreen/ServingTestSuite.java
+++ phase2/src/ServingTableScreen/ServingTestSuite.java
@@ -1,19 +1,38 @@
 package ServingTableScreen;
 
+import Restaurant.*;
+import TablesScreen.TablesScreen;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
 
 public class ServingTestSuite extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("ServingTableScreen.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+//        Parent root = FXMLLoader.load(getClass().getResource("ServingTableScreen.fxml"));
+//        primaryStage.setTitle("ServingScreen");
+//        primaryStage.setScene(new Scene(root, 500, 500));
+//
+//        primaryStage.show();
+        Stage window = new Stage();
+        FXMLLoader loader = new  FXMLLoader(getClass().getResource("ServingTableScreen.fxml"));
+        Parent root = loader.load();
+
+        ServingScreen controller = loader.getController();
+
+        ArrayList<Dish> dishes = new ArrayList<>();
+
+        controller.setCookTable(dishes);
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle("Serving Screen");
+        window.setScene(new Scene(root));
+        window.show();
     }
 
 
