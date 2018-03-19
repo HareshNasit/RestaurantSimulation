93b51a3666d3b4925700a35af4017c8a842540b0
Adam Zartin <A.Zartin@mail.utoronto.ca>
2018-03-19 13:40:39 -0400

    Started To Implement DishStatus class

---
+++ phase2/src/Restaurant/DishStatus.java
@@ -0,0 +1,37 @@
+package Restaurant;
+
+public enum DishStatus {
+    COOKING{
+        @Override
+        public String toString() {
+            return "Cooking";
+        }
+    },
+    PICKUP{
+        @Override
+        public String toString() {
+            return "Pick Up";
+        }
+
+    },
+    REJECTED{
+        @Override
+        public String toString() {
+            return "Rejected";
+        }
+    },
+    RETURNED{
+        @Override
+        public String toString() {
+            return "Returned";
+        }
+    },
+    SERVED{
+        @Override
+        public String toString() {
+            return "Serving";
+        }
+    };
+
+
+}
