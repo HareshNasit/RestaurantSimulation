774242f2914d665776a13ab1acbb68a8c5531c47
Yosef <yosef.leibman@mail.utoronto.ca>
2018-03-19 19:51:22 -0400

    Added some empty methods to the BillScreen class. One is generateReceipts and the other is initializable.

--- phase2/src/BillScreen/BillController.java
+++ phase2/src/BillScreen/BillController.java
@@ -1,4 +1,31 @@
 package BillScreen;
 
-public class BillController {
+import Restaurant.Dish;
+import javafx.fxml.Initializable;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class BillController implements Initializable {
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+
+    public void generateReceipt(ArrayList<Dish> dishes){
+        String fileName = "";
+        try (PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(fileName)))) {
+
+        }catch(Exception e){
+        }
+    }
+
+
 }
--- phase2/src/BillScreen/BillScreen.java
+++ phase2/src/BillScreen/BillScreen.java
@@ -12,7 +12,7 @@ public class BillScreen extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("bill.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
