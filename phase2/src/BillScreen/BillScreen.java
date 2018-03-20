f56e3f4d951a92f14f3771df0904bc7859bfcf9b
Yosef <yosef.leibman@mail.utoronto.ca>
2018-03-20 18:30:57 -0400

    Labels now appear in the drop down menu for the Bill Screen.

--- phase2/src/BillScreen/BillScreen.java
+++ phase2/src/BillScreen/BillScreen.java
@@ -1,5 +1,7 @@
 package BillScreen;
 
+import Restaurant.Menu;
+import Restaurant.Table;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -11,7 +13,21 @@ public class BillScreen extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("bill.fxml"));
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("bill.fxml"));
+        Parent root = loader.load();
+
+        Table table = new Table("A");
+        Menu menu = new Menu();
+        table.addSingleOrder(menu.getDish(1.0, "A", 1));
+        table.addSingleOrder(menu.getDish(2.0, "B", 2));
+        table.addSingleOrder(menu.getDish(2.0, "B", 2));
+        table.addSingleOrder(menu.getDish(3.0, "C", 3));
+
+        BillScreenController controller = loader.getController();
+        table.setTableSize(3);
+        controller.setTable(table);
+
         primaryStage.setTitle("Bill Menu");
         primaryStage.setScene(new Scene(root));
         primaryStage.initStyle(StageStyle.UTILITY);
--- phase2/src/BillScreen/BillScreenController.java
+++ phase2/src/BillScreen/BillScreenController.java
@@ -2,9 +2,11 @@ package BillScreen;
 
 import Restaurant.Bill;
 import Restaurant.Dish;
+import Restaurant.Table;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
+import javafx.scene.control.*;
 import javafx.scene.layout.Region;
 
 import java.io.BufferedWriter;
@@ -13,10 +15,16 @@ import java.io.PrintWriter;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
-public class BillController implements Initializable {
+public class BillScreenController implements Initializable {
 
+
+    public ComboBox scrollMenu;
+    public Button payButton;
+    public Button receiptButton;
+    public Table table;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -44,6 +52,39 @@ public class BillController implements Initializable {
         alert.showAndWait();
     }
 
+    public void setTable(Table table){
+        this.table = table;
+        ArrayList<String> menuLabels = new ArrayList<String>();
+
+        for(int i = 1; i <= table.getTableSize(); i++){
+            menuLabels.add("Customer " + i);
+        }
+        menuLabels.add("All");
+        ObservableList<String> labels = FXCollections.observableArrayList();
+        labels.addAll(menuLabels);
+        scrollMenu.setItems(labels);
+    }
+
+    public double getTip(){
+        ButtonType percent = new ButtonType("%");
+        ButtonType dollarAmount = new ButtonType("$");
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Pick tip type",
+                percent, dollarAmount, ButtonType.CANCEL);
+
+
+        Dialog dialog = new TextInputDialog();
+        dialog.setTitle("Tip Dialog");
+        dialog.setHeaderText("Enter your tip");
+        Optional<String> result = dialog.showAndWait();
+        String entered = "none.";
+
+        if (result.isPresent()) {
+
+            entered = result.get();
+        }
+
+        return 0;
+    }
 
     /**
      *
