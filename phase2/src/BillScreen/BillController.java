88d63a63d1f1710bba0f110e4bcc753c7c8eeff0
Yosef <yosef.leibman@mail.utoronto.ca>
2018-03-19 21:02:50 -0400

    Started developing methods in the BillController that create pop windows for receipts.

--- phase2/src/BillScreen/BillController.java
+++ phase2/src/BillScreen/BillController.java
@@ -1,13 +1,18 @@
 package BillScreen;
 
+import Restaurant.Bill;
 import Restaurant.Dish;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.layout.Region;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 public class BillController implements Initializable {
@@ -19,13 +24,56 @@ public class BillController implements Initializable {
     }
 
 
-    public void generateReceipt(ArrayList<Dish> dishes){
+    /**
+     *
+     * @param dishes Non empty list of the customer's Dishes
+     */
+    public void createCustomerReceiptWindow(ArrayList<Dish> dishes){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, Bill.outputBill(dishes),
+                ButtonType.OK, ButtonType.CANCEL);
+        alert.setTitle("Is this all correct?");
+        alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
+        alert.showAndWait();
+    }
+
+    public void createRestaurantReceiptWindow(ArrayList<Dish> dishes){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, Bill.outputBill(dishes),
+                ButtonType.OK, ButtonType.CANCEL);
+        alert.setTitle("Is this all correct?");
+        alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
+        alert.showAndWait();
+    }
+
+
+    /**
+     *
+     * @param dishes
+     */
+    public void generateCustomerReceiptTxt(ArrayList<Dish> dishes){
+        Date date = new Date();
+
         String fileName = "";
         try (PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(fileName)))) {
 
         }catch(Exception e){
         }
     }
+
+    /**
+     *
+     */
+    public void generateRestaurantReceiptTxt(){
+        Date date = new Date();
+
+        String fileName = "";
+        try (PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(fileName)))) {
+
+        }catch(Exception e){
+        }
+    }
+
+
+
 
 
 }
