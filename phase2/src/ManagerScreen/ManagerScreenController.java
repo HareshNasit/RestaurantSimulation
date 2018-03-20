77d20e02abaacfe7477ffaffea0cf0787006a0fc
Adam Zartin <A.Zartin@mail.utoronto.ca>
2018-03-20 17:06:30 -0400

    -Implemented request.txt tab in manager screen -Manager screen request tab now shows contents to text file

--- phase2/src/ManagerScreen/ManagerScreenController.java
+++ phase2/src/ManagerScreen/ManagerScreenController.java
@@ -3,6 +3,8 @@ package ManagerScreen;
 import Restaurant.Inventory;
 import Restaurant.InventoryIngredient;
 import Restaurant.Table;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
@@ -12,44 +14,44 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 public class ManagerScreenController implements Initializable {
 
+  private final String  REQUESTFILE = "request.txt";
+
+  @FXML
+  private TextArea textFieldRequest;
   @FXML //Allows the variable to be encapsualted
   private Tab tabInventory;
-  @FXML
-  private TableColumn columnIsLow;
-  @FXML
-  private TableColumn columnRestock;
-  @FXML
-  private TableColumn columnLowThresh;
-  @FXML
-  private TableColumn columnAmount;
-  @FXML
-  private TableColumn columnIngredient;
-  @FXML
-  private TableView tableInventory;
+
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
-    columnIngredient.setCellValueFactory(new PropertyValueFactory<Table, String>("name"));
-    columnAmount.setCellValueFactory(new PropertyValueFactory<Table, Integer>("currentQuantity"));
-    columnLowThresh.setCellValueFactory(new PropertyValueFactory<Table, Integer>("lowerThreshold"));
-    columnRestock.setCellValueFactory(new PropertyValueFactory<Table, Integer>("restockQuantity"));
-    getInventoryTableData();
+
+    updateRequestText();
 
   }
 
   private ObservableList<InventoryIngredient> getInventoryTableData() {
     ObservableList<InventoryIngredient> ingredients = FXCollections.observableArrayList();
-    InventoryIngredient ip = new InventoryIngredient("memes", 10, 10, 10);
-    InventoryIngredient jp = new InventoryIngredient("masd", 10, 10, 10);
-    InventoryIngredient yp = new InventoryIngredient("sd", 10, 10, 10);
 
-    ingredients.addAll(ip, jp, yp);
-    tableInventory.setItems(ingredients);
     return ingredients;
+  }
+
+  /**
+   * Updates the request tab in the manager screen with the contents of request.txt
+   */
+  private void updateRequestText(){
+    try (BufferedReader fileReader = new BufferedReader(new FileReader(REQUESTFILE))) {
+      String line = fileReader.readLine();
+      while (line != null) {
+          textFieldRequest.appendText(line + System.lineSeparator());
+          line = fileReader.readLine();
+      }
+    } catch (java.io.IOException e) {
+    }
   }
 
   //---------------------- Getters and Setters --------------------
@@ -62,51 +64,11 @@ public class ManagerScreenController implements Initializable {
     this.tabInventory = tabInventory;
   }
 
-  public TableColumn getColumnIsLow() {
-    return columnIsLow;
+  public TextArea getTextFieldRequest() {
+    return textFieldRequest;
   }
 
-  public void setColumnIsLow(TableColumn columnIsLow) {
-    this.columnIsLow = columnIsLow;
-  }
-
-  public TableColumn getColumnRestock() {
-    return columnRestock;
-  }
-
-  public void setColumnRestock(TableColumn columnRestock) {
-    this.columnRestock = columnRestock;
-  }
-
-  public TableColumn getColumnLowThresh() {
-    return columnLowThresh;
-  }
-
-  public void setColumnLowThresh(TableColumn columnLowThresh) {
-    this.columnLowThresh = columnLowThresh;
-  }
-
-  public TableColumn getColumnAmount() {
-    return columnAmount;
-  }
-
-  public void setColumnAmount(TableColumn columnAmount) {
-    this.columnAmount = columnAmount;
-  }
-
-  public TableColumn getColumnIngredient() {
-    return columnIngredient;
-  }
-
-  public void setColumnIngredient(TableColumn columnIngredient) {
-    this.columnIngredient = columnIngredient;
-  }
-
-  public TableView getTableInventory() {
-    return tableInventory;
-  }
-
-  public void setTableInventory(TableView tableInventory) {
-    this.tableInventory = tableInventory;
+  public void setTextFieldRequest(TextArea textFieldRequest) {
+    this.textFieldRequest = textFieldRequest;
   }
 }
