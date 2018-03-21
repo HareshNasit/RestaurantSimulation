5be806f48a1832cbfa5f65085ddac194e546dca1
Adam Zartin <A.Zartin@mail.utoronto.ca>
2018-03-21 17:11:59 -0400

    -Implemented scenes to connect windows in tables

--- phase2/src/TablesScreen/TablesScreen.java
+++ phase2/src/TablesScreen/TablesScreen.java
@@ -1,11 +1,16 @@
 package TablesScreen;
 
 import Restaurant.Table;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TabPane;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -15,6 +20,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.HBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 
 /**
@@ -53,30 +60,39 @@ public class TablesScreen implements EventHandler<ActionEvent>, Initializable {
   /**
    * Opens the selected restaurant table's menu from the TablesScreen tables
    */
-  public void openTableMenu(){
-
-    try {
-      Table table = getTableView().getSelectionModel().getSelectedItem();
-      System.out.println(String.format("Open Table (%s) Menu.......", table.getTableID()));
-    } catch (NullPointerException e){
-      System.out.println("Select a table bro");
+  public void openTableMenu() throws IOException{
+    Table table = getTableView().getSelectionModel().getSelectedItem();
+    try{FXMLLoader loader = new  FXMLLoader(getClass().getResource("../OrderScreen/orders.fxml"));
+      Parent root = loader.load();
+      hBox.getChildren().setAll(root);
+    } catch (IOException e){
 
     }
-    
+
+
   }
 
   /**
    * Opens the serving table for the server to see
    */
-  public void openServingTable(){
+  public void openServingTable() throws IOException{
     System.out.println("Loading Serving Table.....");
+    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(
+        "../ServingTableScreen/ServingTableScreen.fxml"));
+    Parent root1 = fxmlLoader.load();
+    hBox.getChildren().setAll(root1);
   }
 
   /**
    * Calls the manager for special events
    */
-  public void callManager(){
+  public void callManager() throws IOException {
     System.out.println("Calling Manager......");
+
+    FXMLLoader loader = new  FXMLLoader(getClass().getResource("ManagerScreen.ManagerScreen.fxml"));
+    TabPane root = loader.load();
+    hBox.getChildren().setAll(root);
+
   }
 
   /**
@@ -106,6 +122,10 @@ public class TablesScreen implements EventHandler<ActionEvent>, Initializable {
       System.out.println("more memes");
     }
   }
+
+  //------------------------- GETTERS AND SETTERS BELOW --------------------
+  //Probs remove setters
+  //Getters are needed to generate
 
   public TableView<Table> getTableView() {
     return tableView;
