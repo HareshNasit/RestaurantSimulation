a9f42907aa7bf8985c57c931f62fd4fa8635b089
Yosef <yosef.leibman@mail.utoronto.ca>
2018-03-23 20:20:40 -0400

    Finished all necessary methods in bill. Manager can now create a new Receiptfile at the start of the day.

--- phase2/src/Restaurant/Bill.java
+++ phase2/src/Restaurant/Bill.java
@@ -1,15 +1,12 @@
 package Restaurant;
 
-// import com.sun.org.apache.xpath.internal.operations.Bool;
-
-import java.lang.reflect.Array;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.ArrayList;
 
 public class Bill {
 
-    public static double tax = .13;
+    public static double salestax = .13;
 
     /**
      * Creates a bill from a given set of orders. This bill will be used for creating a full bill
@@ -26,20 +23,10 @@ public class Bill {
         }
 
         double subtotal = getSubtotal(orders);
-        double tax = getTax(subtotal);
-        billText += "SubTotal: $" + subtotal  + System.lineSeparator();
-        billText += "Tax: $" + tax  + System.lineSeparator();
-
-        BigDecimal bd = new BigDecimal(subtotal);
-        BigDecimal bigTax = new BigDecimal(tax);
-        bd = bd.add(bigTax);
-        bd = bd.setScale(2, RoundingMode.CEILING);
-        double total = bd.doubleValue();
-
-
-        billText += "Total: $" + total  + System.lineSeparator();
+        billText += billTextHelper(subtotal);
         return billText;
     }
+
 
     /**
      * Creates a bill for one customer at a table.
@@ -55,82 +42,98 @@ public class Bill {
         for (Dish order : table.getCustomerOrder(customerNum)) {
             billText += order.getStringForBill() + System.lineSeparator();
         }
-
         double subtotal = getSubtotal(table.getCustomerOrder(customerNum));
-        double tax = getTax(subtotal);
-        BigDecimal bd = new BigDecimal(subtotal);
-        BigDecimal bigTax = new BigDecimal(tax);
-        bd = bd.add(bigTax);
-        bd = bd.setScale(2, RoundingMode.CEILING);
-        double total = bd.doubleValue();
-
-        billText += "SubTotal: $" + subtotal + System.lineSeparator();
-        billText += "Tax: $" + tax + System.lineSeparator();
-        billText += "Total: $" + total  + System.lineSeparator();
+        billText += billTextHelper(subtotal);
         return billText;
     }
 
-    public static double getSubtotal(ArrayList<Dish> dishes) {
-        double subtotal = 0;
-        for (Dish order : dishes) {
-            subtotal += order.getPrice();
-        }
-        return subtotal;
-    }
 
-    public static double getTax(double subtotal) {
-        BigDecimal bd = new BigDecimal(subtotal);
-        BigDecimal multiplier = new BigDecimal(tax);
-        bd = bd.multiply(multiplier);
-        bd = bd.setScale(2, RoundingMode.CEILING);
-
-        return bd.doubleValue();
-    }
-
-
-    public static String outSinglePaymentBill(Table table, int customerNum, Boolean isPercent, double tip){
+    /**
+     * String method for the final payment including a $ amount tip.
+     *
+     * @param table
+     * @param customerNum
+     * @param tip
+     * @return
+     */
+    public static String finalPaymentSingleTip(Table table, int customerNum, double tip) {
         String billText = "";
         billText += "Table: " + table.getTableID() + ", ";
         billText += "CustomerNumber: " + customerNum + ", ";
         for (Dish order : table.getCustomerOrder(customerNum)) {
             billText += order.getStringForBill() + System.lineSeparator();
         }
-
+        double tipAmount = get2Decimals(tip);
         double subtotal = getSubtotal(table.getCustomerOrder(customerNum));
         double tax = getTax(subtotal);
-        BigDecimal bd = new BigDecimal(subtotal);
-        BigDecimal bigTax = new BigDecimal(tax);
-        bd = bd.add(bigTax);
-        bd = bd.setScale(2, RoundingMode.CEILING);
-        double total = bd.doubleValue();
-
+        double total = getTotal(subtotal + tip, tax);
         billText += "SubTotal: $" + subtotal + System.lineSeparator();
+        billText += "Tip: $" + tipAmount + System.lineSeparator();
         billText += "Tax: $" + tax + System.lineSeparator();
-        billText += "Total: $" + total  + System.lineSeparator();
+        billText += "Total: $" + total + System.lineSeparator();
         return billText;
     }
 
-    public static String outputFullTablePaymentBill(ArrayList<Dish> orders, Boolean isPercent, double tip){
+    /**
+     * @param orders
+     * @param tip
+     * @return
+     */
+    public static String finalPaymentBillPercent(ArrayList<Dish> orders, double tip) {
         String billText = "";
-
         for (Dish order : orders) {
             billText += order.getStringForBill() + System.lineSeparator();
         }
-
         double subtotal = getSubtotal(orders);
         double tax = getTax(subtotal);
-        billText += "SubTotal: $" + subtotal  + System.lineSeparator();
-        billText += "Tax: $" + tax  + System.lineSeparator();
+        double tipAmount = get2Decimals(subtotal * (1 + tip / 100));
+        double total = getTotal(subtotal + tipAmount, tax);
+        billText += "SubTotal: $" + subtotal + System.lineSeparator();
+        billText += "Tax: $" + tax + System.lineSeparator();
+        billText += "Tip: $" + tipAmount + System.lineSeparator();
+        billText += "Total: $" + total + System.lineSeparator();
+        return billText;
+    }
 
+    private static double get2Decimals(double number) {
+        BigDecimal bd = new BigDecimal(number);
+        bd = bd.setScale(2, RoundingMode.CEILING);
+        return bd.doubleValue();
+    }
+
+    public static double getTotal(double subtotal, double tax) {
         BigDecimal bd = new BigDecimal(subtotal);
         BigDecimal bigTax = new BigDecimal(tax);
         bd = bd.add(bigTax);
-        bd = bd.setScale(2, RoundingMode.CEILING);
-        double total = bd.doubleValue();
+        return get2Decimals(bd.doubleValue());
+    }
+
+    public static double getTax(double subtotal) {
+        BigDecimal bd = new BigDecimal(subtotal);
+        BigDecimal multiplier = new BigDecimal(salestax);
+        bd = bd.multiply(multiplier);
+        return get2Decimals(bd.doubleValue());
+    }
 
 
-        billText += "Total: $" + total  + System.lineSeparator();
+    public static double getSubtotal(ArrayList<Dish> dishes) {
+        double subtotal = 0;
+        for (Dish order : dishes) {
+            subtotal += order.getPrice();
+        }
+        return get2Decimals(subtotal);
+    }
+
+    private static String billTextHelper(double subtotal) {
+        String billText = "";
+        double tax = getTax(subtotal);
+        double total = getTotal(subtotal, tax);
+
+        billText += "SubTotal: $" + subtotal + System.lineSeparator();
+        billText += "Tax: $" + tax + System.lineSeparator();
+        billText += "Total: $" + total + System.lineSeparator();
         return billText;
     }
+
 
 }
--- phase2/src/Restaurant/Manager.java
+++ phase2/src/Restaurant/Manager.java
@@ -1,6 +1,11 @@
 package Restaurant;
 
+import sun.util.calendar.BaseCalendar;
+
 import java.io.*;
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /*
  * A Manager manages the overall restaurant and gets the ingredients if there
@@ -8,63 +13,82 @@ import java.io.*;
  */
 public class Manager implements IWorker, InventoryListener {
 
-  private String name;
-  private final String RECEIVEDFILE = "receivedShipments.txt";
+    private String name;
+    private final String RECEIVEDFILE = "receivedShipments.txt";
 
-  public Manager(String name) {
-    this.name = name;
-  }
-
-  @Override
-  public String getName() {
-    return name;
-  }
-
-  @Override
-  public String getType() {
-    return "Manager";
-  }
-
-  /** Calls a server or a cook to collect the received ingredients. */
-  public IWorker callWorker(Restaurant restaurant, String name) {
-    return restaurant.MapOfWorkers().get(name);
-  }
-
-  @Override
-  public void notifyLowStock(String message) {
-    // to be replaced with gui notification
-    System.out.println(message);
-  }
-
-  /** The server receives and adds ingredients to the inventory. */
-  public void scanStock(Inventory inventory, String ingredient, int amount) {
-    inventory.addStock(ingredient, amount);
-  }
-
-  /** The Manager shuts the system and updates the inventory.txt file. */
-  public void shutDown(Inventory inventory) {
-    System.out.println("System Shutdown: Writing inventory to file");
-    inventory.writeToInventory();
-  }
-
-  /**
-   * The Manager inputs how much of an ingredient the Restaurant has received. This gets printed to
-   * a new txt. The manager will compare between the request txt and receivedShipments txt to see
-   * which shipments have been received. This is useful if a shipment has not come in, so the
-   * manager can send an email to the supplier.
-   *
-   * @param ingredient ingredient that has been received
-   * @param amount amount of ingredient that is received
-   */
-  public void confirmReceived(String ingredient, int amount) {
-    try (PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(RECEIVEDFILE)))) {
-
-      String shipmentMessage = ingredient + ": " + amount;
-      out.println(shipmentMessage);
-
-    } catch (Exception e) {
-
+    public Manager(String name) {
+        this.name = name;
     }
-  }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getType() {
+        return "Manager";
+    }
+
+    /**
+     * Calls a server or a cook to collect the received ingredients.
+     */
+    public IWorker callWorker(Restaurant restaurant, String name) {
+        return restaurant.MapOfWorkers().get(name);
+    }
+
+    @Override
+    public void notifyLowStock(String message) {
+        // to be replaced with gui notification
+        System.out.println(message);
+    }
+
+    /**
+     * The server receives and adds ingredients to the inventory.
+     */
+    public void scanStock(Inventory inventory, String ingredient, int amount) {
+        inventory.addStock(ingredient, amount);
+    }
+
+    /**
+     * The Manager shuts the system and updates the inventory.txt file.
+     */
+    public void shutDown(Inventory inventory) {
+        System.out.println("System Shutdown: Writing inventory to file");
+        inventory.writeToInventory();
+    }
+
+    /**
+     * The Manager inputs how much of an ingredient the Restaurant has received. This gets printed to
+     * a new txt. The manager will compare between the request txt and receivedShipments txt to see
+     * which shipments have been received. This is useful if a shipment has not come in, so the
+     * manager can send an email to the supplier.
+     *
+     * @param ingredient ingredient that has been received
+     * @param amount     amount of ingredient that is received
+     */
+    public void confirmReceived(String ingredient, int amount) {
+        try (PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(RECEIVEDFILE)))) {
+
+            String shipmentMessage = ingredient + ": " + amount;
+            out.println(shipmentMessage);
+
+        } catch (Exception e) {
+
+        }
+    }
+
+    /**
+     * Creates the receipt txt that keeps track of all the payments/bills.
+     *
+     * @param restaurant
+     */
+    public void createNewReceiptFile(Restaurant restaurant) {
+        String pattern = "dd-MM-yyyy";
+        String dateInString = new SimpleDateFormat(pattern).format(new Date());
+        String name = "receipt" + dateInString + ".txt";
+        File file = new File("/receipts/" + name);
+        restaurant.setRECEIPTFILE(name);
+    }
 
 }
