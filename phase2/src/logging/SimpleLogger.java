4287544ecf867099be2d561e14c9943126efe1c1
Yosef <yosef.leibman@mail.utoronto.ca>
2018-03-25 19:37:10 -0400

    Created a Simple Logging System class.

---
+++ phase2/src/logging/SimpleLogger.java
@@ -0,0 +1,59 @@
+package logging;
+
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class SimpleLogger {
+
+    String loggerDestination;
+
+    public SimpleLogger(String filePath) {
+        this.loggerDestination = filePath;
+    }
+
+    public void setLoggerDestination(String filePath) {
+        this.loggerDestination = filePath;
+    }
+
+    /**
+     * Creates the logger file w/ date
+     */
+    public void createAndSetNewLoggerFile(String filePath, String namePrefix) {
+        String pattern = "dd-MM-yyyy";
+        String dateInString = new SimpleDateFormat(pattern).format(new Date());
+        String name = filePath + namePrefix + dateInString + ".txt";
+
+        File file = new File(name);
+
+        try {
+            PrintWriter writer = new PrintWriter(name, "UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        setLoggerDestination(name);
+    }
+
+    public void writeToLogger(String content) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(loggerDestination, true));
+            writer.write(content);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String returnAllContent() {
+        String info = "";
+        try (BufferedReader reader = new BufferedReader(new FileReader(new File(loggerDestination)))) {
+            String line;
+            while ((line = reader.readLine()) != null)
+                info += line + System.lineSeparator();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return info;
+    }
+}
