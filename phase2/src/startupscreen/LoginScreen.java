5513253a053536ce7d2501399c23ae899b7ea9f6
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2018-03-25 22:53:58 -0400

    Login for manager implemented.

--- phase2/src/startupscreen/LoginScreen.java
+++ phase2/src/startupscreen/LoginScreen.java
@@ -6,9 +6,11 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 
 import java.awt.event.ActionEvent;
@@ -23,6 +25,7 @@ public class LoginScreen implements Initializable{
     public Button serverBtn;
     public PasswordField password;
     public TextField userName;
+    public Label canLogin;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -44,6 +47,17 @@ public class LoginScreen implements Initializable{
             mainScreen.show();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public void checkLogin(javafx.event.ActionEvent actionEvent) {
+        if(userName.getText().equals("harsh") && password.getText().equals("halamadrid")){
+            canLogin.setText("Login successfull");
+            canLogin.setTextFill(Paint.valueOf("Green"));
+        }
+        else{
+            canLogin.setText("Sorry, login Unsuccessful");
+            canLogin.setTextFill(Paint.valueOf("Red"));
         }
     }
 }
