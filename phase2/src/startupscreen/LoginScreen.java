fc1d8cd66dfd65aa813ece7ebdcfd63fbccad848
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2018-03-25 22:44:28 -0400

    New fxml file created for manager login and buttons and labels for login screen added for manager and code for opening 2 screens implemented.

--- phase2/src/startupscreen/LoginScreen.java
+++ phase2/src/startupscreen/LoginScreen.java
@@ -1,11 +1,49 @@
 package startupscreen;
 
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
-public class LoginScreen {
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginScreen implements Initializable{
 
     public Button managerBtn;
     public Button cookBtn;
     public Button serverBtn;
+    public PasswordField password;
+    public TextField userName;
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void cookLoginScreen(MouseEvent mouseEvent) {
+    }
+
+    public void serverLoginScreen(MouseEvent mouseEvent) {
+    }
+
+    public void managerLoginScreen(javafx.event.ActionEvent actionEvent) {
+        try {
+            Parent managerScreen = FXMLLoader.load(getClass().getResource("ManagerLogin.fxml"));
+            Scene managerScene = new Scene(managerScreen);
+            Stage mainScreen = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            mainScreen.setScene(managerScene);
+            mainScreen.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
