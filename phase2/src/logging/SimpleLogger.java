71502a79d77c9c49401b1179b5de457975e35bbd
Yosef <yosef.leibman@mail.utoronto.ca>
2018-03-26 12:50:49 -0400

    Got rid of checkstyle warnings in SimpleLogger and RestaurantLogger.

--- phase2/src/logging/SimpleLogger.java
+++ phase2/src/logging/SimpleLogger.java
@@ -1,94 +1,101 @@
 package logging;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class SimpleLogger {
 
-    String loggerDestination;
+  String loggerDestination;
 
-    /**
-     * SimpleLogger constructor.
-     *
-     * @param filePath filePath for the logger.
-     */
-    public SimpleLogger(String filePath) {
-        this.loggerDestination = filePath;
+  /**
+   * SimpleLogger constructor.
+   *
+   * @param filePath filePath for the logger.
+   */
+  public SimpleLogger(String filePath) {
+    this.loggerDestination = filePath;
+  }
+
+
+  /**
+   * Creates the logger file w/ date.
+   */
+  public void createAndSetNewLoggerFile(String filePath, String namePrefix) {
+    String pattern = "dd-MM-yyyy";
+    String dateInString = new SimpleDateFormat(pattern).format(new Date());
+    String name = filePath + "/" + namePrefix + dateInString + ".txt";
+
+    File file = new File(name);
+    setLoggerDestination(name);
+
+    try {
+      PrintWriter writer = new PrintWriter(name, "UTF-8");
+    } catch (IOException e) {
+      e.printStackTrace();
     }
 
+  }
 
-    /**
-     * Creates the logger file w/ date
-     */
-    public void createAndSetNewLoggerFile(String filePath, String namePrefix) {
-        String pattern = "dd-MM-yyyy";
-        String dateInString = new SimpleDateFormat(pattern).format(new Date());
-        String name = filePath + "/" + namePrefix + dateInString + ".txt";
+  /**
+   * Writes a message to the log.
+   *
+   * @param content information being written to the logger.
+   */
+  public void writeToLogger(String content) {
 
-        File file = new File(name);
-        setLoggerDestination(name);
+    try {
+      BufferedWriter writer = new BufferedWriter(new FileWriter(this.loggerDestination, true));
+      writer.write(content);
+      writer.close();
+    } catch (IOException e) {
 
-
-        try {
-            PrintWriter writer = new PrintWriter(name, "UTF-8");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+      e.printStackTrace();
     }
+  }
 
-    /**
-     * Writes a message to the log.
-     *
-     * @param content information being written to the logger.
-     */
-    public void writeToLogger(String content) {
+  /**
+   * Returns a string with all the info from the logger.
+   *
+   * @return String with all info from the logger (separated by line).
+   */
+  public String returnAllContent() {
+    String info = "";
+    try (BufferedReader reader = new BufferedReader(new FileReader(new File(loggerDestination)))) {
+      String line;
+      while ((line = reader.readLine()) != null) {
+        info += line + System.lineSeparator();
+      }
 
-        try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter(this.loggerDestination, true));
-            writer.write(content);
-            writer.close();
-        } catch (IOException e) {
-
-            e.printStackTrace();
-        }
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+    return info;
+  }
 
-    /**
-     * Returns a string with all the info from the logger.
-     *
-     * @return String with all info from the logger (separated by line).
-     */
-    public String returnAllContent() {
-        String info = "";
-        try (BufferedReader reader = new BufferedReader(new FileReader(new File(loggerDestination)))) {
-            String line;
-            while ((line = reader.readLine()) != null)
-                info += line + System.lineSeparator();
+  /**
+   * Setter for the filePath.
+   *
+   * @param filePath filePath of the logger.
+   */
+  public void setLoggerDestination(String filePath) {
+    this.loggerDestination = filePath;
+  }
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return info;
-    }
-
-    /**
-     * Setter for the filePath
-     *
-     * @param filePath filePath of the logger.
-     */
-    public void setLoggerDestination(String filePath) {
-        this.loggerDestination = filePath;
-    }
-
-    /**
-     * Getter for the filePath
-     *
-     * @return filePath of the logger.
-     */
-    public String getLoggerDestination() {
-        return this.loggerDestination;
-    }
+  /**
+   * Getter for the filePath.
+   *
+   * @return filePath of the logger.
+   */
+  public String getLoggerDestination() {
+    return this.loggerDestination;
+  }
 
 }
