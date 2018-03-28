434b4849fd169ef9f961ebb081780cd1880585c0
Yosef <yosef.leibman@mail.utoronto.ca>
2018-03-28 18:42:28 -0400

    Server can now add Compliments to dishes and open a billscreen menu.

--- phase2/src/OrderScreen/OrderScreen.java
+++ phase2/src/OrderScreen/OrderScreen.java
@@ -9,6 +9,7 @@ import Restaurant.Table;
 import Restaurant.MenuItem;
 import Restaurant.ModelControllerInterface;
 import TablesScreen.TablesScreen;
+// import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -29,7 +30,6 @@ import java.util.ArrayList;
 import java.util.Optional;
 import java.util.ResourceBundle;
 import javafx.scene.layout.VBox;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
 import notification.Notification;
 
