d951a8ef516ea9636b48ded896adcbe8b168f25c
Akarsh <akarsh.sharma@mail.utoronto.ca.here>
2018-03-29 18:37:07 -0400

    Added javadocs for class ModelControllerInterface.

--- phase2/src/Restaurant/ModelControllerInterface.java
+++ phase2/src/Restaurant/ModelControllerInterface.java
@@ -5,10 +5,23 @@ package Restaurant;
  */
 public interface ModelControllerInterface {
 
+  /**
+   * Updates the screens to make sure all items that appear on the  screen are up to date 
+   */
   public void updateScreen();
 
+  /**
+   * Sends a notification to the screen that the user is on
+   * @param message the message that is being sent
+   */
   public void openNotification(String message);
 
+  /**
+   * opens the receiver function which allows workers to collect stock when they click it
+   * @param inventory
+   * @param ingredient
+   * @param amount
+   */
   public void openReceiverFunction(Inventory inventory, String ingredient, int amount);
 
 }
