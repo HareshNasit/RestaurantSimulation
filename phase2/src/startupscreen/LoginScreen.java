c84506c81ed38fe6f4c4680dacf054ba094d246d
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2018-03-29 15:07:48 -0400

    Workers file read only once in login controller.

--- phase2/src/startupscreen/LoginScreen.java
+++ phase2/src/startupscreen/LoginScreen.java
@@ -24,85 +24,31 @@ import java.util.ResourceBundle;
 
 public class LoginScreen implements Initializable{
 
-    public Button managerBtn;
-    public Button cookBtn;
-    public Button serverBtn;
     public PasswordField password;
     public TextField userName;
     public Label canLogin;
-    public Button cookLoginBtn;
-    public TextField cookNameLbl;
-    public AnchorPane loginScreenAnchorPane;
     public Button loginBtn;
     public AnchorPane workerLoginScreenAnchorPane;
     private boolean systemOn = false;
     private final String WORKERS = "workers.txt";
 
     @Override
-    public void initialize(URL location, ResourceBundle resources) {
-
-    }
-
-    public void cookLoginScreen(javafx.event.ActionEvent mouseEvent) {
-        try {
-            Parent managerScreen = FXMLLoader.load(getClass().getResource("loginScreen.fxml"));
-            Scene managerScene = new Scene(managerScreen);
-            Stage mainScreen = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
-            Button back1 = new Button();
-            back1.setOnAction(event -> {
-                LoginScreen loginScreen = new LoginScreen();
-                loginScreenAnchorPane.getChildren().setAll((Collection<? extends Node>) loginScreen);
-            });
-            back1.setLayoutX(50);
-            back1.setLayoutY(10);
-            workerLoginScreenAnchorPane.getChildren().add(back1);
-            mainScreen.setScene(managerScene);
-            mainScreen.show();
-            
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void serverLoginScreen(ActionEvent mouseEvent) {
-        try {
-            Parent managerScreen = FXMLLoader.load(getClass().getResource("loginScreen.fxml"));
-            Scene managerScene = new Scene(managerScreen);
-            Stage mainScreen = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
-            mainScreen.setScene(managerScene);
-            mainScreen.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void workerLoginScreen(javafx.event.ActionEvent actionEvent) {
-        try {
-            Parent managerScreen = FXMLLoader.load(getClass().getResource("loginScreen.fxml"));
-            Scene managerScene = new Scene(managerScreen);
-            Stage mainScreen = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-            mainScreen.setScene(managerScene);
-            mainScreen.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void checkCookLogin(javafx.event.ActionEvent actionEvent) {
-        if(cookNameLbl.getText().equals("harsh")){
-            System.out.println("yayyy login successful");
-        }
-    }
-
+    public void initialize(URL location, ResourceBundle resources) {}
     public void checkWorkerLogin(ActionEvent actionEvent) {
         try (BufferedReader fileReader = new BufferedReader(new FileReader(WORKERS))) {
             String line = fileReader.readLine();
+            ArrayList workers = new ArrayList();
             while (line != null) {
                 String[] details = line.split("\\|");
-                if(checkUserIds(userName.getText(),password.getText(),details)) {
+                workers.add(details);
+                line = fileReader.readLine();
+            }
+            for(Object details: workers) {
+
+                if(checkUserIds(userName.getText(),password.getText(),(String[]) details)) {
                     System.out.println("Yallaa it workssss");
 
-                    if (details[0].equals("Manager")) {
+                    if (((String[])details)[0].equals("Manager")) {
                         canLogin.setText("Logged in");
                         canLogin.setTextFill(Paint.valueOf("Green"));
                         systemOn = true;
@@ -121,7 +67,6 @@ public class LoginScreen implements Initializable{
                     canLogin.setText("Sorry wrong username or pass");
                     canLogin.setTextFill(Paint.valueOf("Red"));
                 }
-                line = fileReader.readLine();
             }
         } catch (java.io.FileNotFoundException ex) {
             System.out.println(
@@ -133,7 +78,7 @@ public class LoginScreen implements Initializable{
                             + "'");
         }
     }
-        public boolean checkUserIds(String username, String password, String[] details){
+        private boolean checkUserIds(String username, String password, String[] details){
             if((details[1].equals(username)) && (details[2].equals(password))){
                 return true;
             }
